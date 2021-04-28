1d91a6b95c5c69897d8af89c0f976f071670beb2
franky4p_mac <franky4p@yandex.ru>
2021-04-28 20:26:22 +0300

    DZ3

--- Lesson2/Lesson2/main.m
+++ Lesson2/Lesson2/main.m
@@ -7,6 +7,8 @@
 
 #import <Foundation/Foundation.h>
 
+//MARK: - Task 2
+
 BOOL itsEng(NSString* string) {
     NSString *engChar = @"abcdefghijklmnopqrstuvwxyz";
     return [engChar rangeOfString:string].length ? YES : NO;
@@ -31,6 +33,78 @@ int division(int a, int b) {
     return a / b;
 }
 
+//MARK: - Task 3
+
+void loop() {
+    NSArray *myArray = @[@1, @2, @3, @4, @5];
+    
+    for (int i = 0; i < 4; i++) {
+        NSLog(@"%@", myArray[i]);
+    }
+    
+    NSLog(@"While");
+    
+    int i = 0;
+    while (i < myArray.count) {
+        NSLog(@"%@", myArray[i]);
+        i+=1;
+    }
+    
+    NSLog(@"Do While");
+    
+    i = 0;
+    do {
+        NSLog(@"%@", myArray[i]);
+        i += 1;
+    } while (i < myArray.count);
+}
+
+
+enum Operation {
+    OperationAdd,
+    OperationSub,
+    OperationMultiplication,
+    OperationDivision
+};
+
+typedef enum Operation Operation;
+
+int calculate(Operation operation, int a, int b) {
+    int rezult = 0;
+    
+    switch (operation) {
+        case OperationAdd:
+            rezult = add(a, b);
+            break;
+        case OperationSub:
+            rezult = sub(a, b);
+            break;
+        case OperationMultiplication:
+            rezult = multiplication(a, b);
+            break;
+        case OperationDivision:
+            rezult = division(a, b);
+            break;
+        default:
+            break;
+    }
+    
+    return rezult;
+}
+
+enum Gander {
+    GanderM,
+    GanderF
+};
+
+struct Human {
+    NSInteger age;
+    NSString *name;
+    enum Gander gander;
+};
+
+typedef struct Human Human;
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
         //task 1
@@ -40,6 +114,28 @@ int main(int argc, const char * argv[]) {
         //task 2
         int rezult = sub(8, 2);
         NSLog(@"%i", rezult);
+        
+        //DZ3
+        NSLog(@"----DZ3----");
+        
+        loop();
+        
+        int rezult1 = calculate(OperationSub, 8, 2);
+        NSLog(@"%i", rezult1);
+        
+        Human human1;
+        human1.age = 20;
+        human1.name = @"Jon";
+        human1.gander = GanderM;
+        
+        Human human2;
+        human2.age = 21;
+        human2.name = @"Jane";
+        human2.gander = GanderF;
+        
+        NSLog(@"Human1: \n name %@, \n gander %u, \n age %ld, \n", human1.name, human1.gander, (long)human1.age);
+
+        NSLog(@"Human2: \n name %@, \n gander %u, \n age %ld, \n", human2.name, human2.gander, (long)human2.age);
     }
     return 0;
 }
